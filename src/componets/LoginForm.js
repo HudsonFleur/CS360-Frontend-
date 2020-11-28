d4422a6a7106fdb3a38d10700e918bbfedabc4cf
Hudson Fleurimond <fleurimond003@wcsu.edu>
2020-11-28 16:16:30 -0500

    Login/Register Page now have material UI elements

--- src/componets/LoginForm.js
+++ src/componets/LoginForm.js
@@ -1,90 +1,115 @@
-/*
--------------------- Log File --------------------
-Last Worked on: November 24th   Hudson Fleurimond
-    Known Issues:
-        . 
-
-    What Needs Work:
-        . UI Styling, UI needs an overhaul of course, only implementation has been done
-        . CSS styles get whatever is on the home, We need to give everything a style or CSS components
-            so the default isn't the same thing everywhere,
-        . Adding Alerts and Error Notifications
-
-    Concerns:
-        . Change post method form
-        . Redo Entire page with material ui
-*/
 import React from 'react';
 import Axios from 'axios';
 import {withRouter } from "react-router-dom";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
-// LoginForm class
+/* LoginForm Class */
 class LoginForm extends React.Component
-{   // Constructor for Login Class
+{   /* Constructor for Login Class */
     /*
         State Declarations
-            email:      State object for holding the email of the User, set to empty initially
-            password:   State object for holding the password of the User, set to empty initially
+            user:           State object for holding the User information, the email and password for the user logging in.
+            loginFailed:    State variable for controling the dialog form for errors when a user attempts to login.
     */
     constructor(props) 
     {
         super(props);
         this.state = {
-            email: "",
-            password: ""
+            user:{
+                email: '',
+                password: ''
+            },
+            loginFailed: false
         }
-        this.getEmail = this.getEmail.bind(this);
-        this.getPassword = this.getPassword.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
-    // Function to get the User Email
-    getEmail(event) {
-        this.setState({ email: event.target.value });
+    /* Function to get the User Email */
+    getEmail = (event) => {
+        this.setState({ user:{ ...this.state.user, email: event.target.value}})
     }
-    // Function to get the User Password
-    getPassword(event) {
-        this.setState({ password: event.target.value });
+    /* Function to get the User Password */
+    getPassword = (event) => {
+        this.setState({ user:{ ...this.state.user, password: event.target.value}})
     }
+    /*
+        This function is responsible for updating the loginFailed state.
+    */
+    closeDialog = () => {
+        this.setState({ loginFailed: false });
+    };
     /* Function to send a POST request to the database (server), create a person object
         which contains the User's Email and User's Password and POST to the route for
         handling user logins. Upon successful Login, redirect to the Task's Page
         and send along the response.
     */
-    handleSubmit (event)
+   
+    handleSubmit = () =>
     {
-        event.preventDefault();
         const { history } = this.props;
 
-        const person = {
-            email: this.state.email,
-            password: this.state.password
-        };
-        
-        Axios.post('http://localhost:5000/users/login', person)
-            .then(function(response) {
-                history.push({
-                    pathname:'/tasks',
-                    response
-                });
-            })
-            .catch(function(error) {});
-
-        this.setState({ email: "", password: "" })
+        Axios({
+            method: 'POST',
+            url: 'http://localhost:5000/users/login',
+            data: this.state.user
+        }).then(function(response) {
+            if(response.status === 200)
+            {
+                history.push({pathname:'/tasks', response});
+            }
+        }).catch((error) =>
+        {
+            if(error.response.status === 400)
+            {
+                this.setState({loginFailed: true});
+            }
+        })
     }
+
     render()
     {
         return (
             <div>
-            <form onSubmit = {this.handleSubmit} >
-                <label >
-                    Email: <input type = "text" value = { this.state.email } onChange = { this.getEmail }/> 
-                </label> 
-                
-                <label >
-                    Password: <input type = "password" value = { this.state.password } onChange = { this.getPassword }/> 
-                </label>    
-                <input type = "submit" value = "Login" />
-            </form> 
+                <div>
+                    <TextField
+                        label="Email"
+                        style={{ margin: 8, width: '35ch' }}
+                        placeholder="Email"
+                        value={this.state.user.email}
+                        onChange={this.getEmail}
+                        margin="normal"
+                        type="text"/>
+                </div>
+                <div>
+                    <TextField
+                        label="Password"
+                        style={{ margin: 8, width: '35ch' }}
+                        placeholder="Password"
+                        value={this.state.user.password}
+                        onChange={this.getPassword}
+                        margin="normal"
+                        type="password"/> 
+                </div>
+                <div>
+                    <Button onClick={() => this.handleSubmit()}>
+                            Login
+                    </Button>
+                </div>
+
+                <Dialog open={this.state.loginFailed} onClose={this.closeDialog} aria-labelledby="form-dialog-title">
+                    <DialogTitle>Error</DialogTitle>
+                    <DialogContent> 
+                        The username or password is incorrect.
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.closeDialog}>
+                            Ok.
+                        </Button>
+                    </DialogActions>
+                </Dialog>
         </div>
         )
     }
--- src/componets/RegisterForm.jsx
+++ src/componets/RegisterForm.jsx
@@ -1,103 +1,229 @@
-/*
--------------------- Log File --------------------
-Last Worked on: November 24th   Hudson Fleurimond
-    Known Issues:
-        . 
-
-    What Needs Work:
-        . UI Styling, UI needs an overhaul of course, only implementation has been done
-        . CSS styles get whatever is on the home, We need to give everything a style or CSS components
-            so the default isn't the same thing everywhere,
-        . Adding Alerts and Error Notifications
-
-    Concerns:
-        . Change post method form
-        . Redo Entire page with material ui
-*/
 import React from 'react'
 import Axios from 'axios'
 import {withRouter} from "react-router-dom";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
-// LoginForm class
+/* RegisterForm Class */
 class RegisterForm extends React.Component {
-    // Constructor for Login Class
+    /* Constructor for RegisterForm Class */
     /*
         State Declarations
-            name:       State object for holding the name of the User, set to empty initially
-            email:      State object for holding the email of the User, set to empty initially
-            password:   State object for holding the password of the User, set to empty initially
+            user:               State object for holding the User information, the name, the email and password for the account registration.
+            confirmPassword:    State variable for holding the confirmation password field.
+            nameValidation:     State variable for controling the dialog form for incorrect name requirements.
+            emailValidation:    State variable for controling the dialog form for incorrect email requirements.
+            passwordValidation:           State variable for controling the dialog form for incorrect password requirements.
+            confirmPasswordValidaiton:    State variable for controling the dialog form for incorrect password requirements.
     */
     constructor(props) {
         super(props);
         this.state = {
-            name: "",
-            email: "",
-            password: "",
+            user:{
+                name: '',
+                email: '',
+                password: ''
+            },
+            confirmPassword: '',
+            nameValidation: false,
+            emailValidation: false,
+            passwordValidation: false,
+            passwordConfirmationValidation: false
         }
-        this.setName = this.setName.bind(this);
-        this.setEmail = this.setEmail.bind(this);
-        this.setPassword = this.setPassword.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
     // Function to set the User Name
-    setName(event) {
-        this.setState({ name: event.target.value });
+    setName = (event) => {
+        this.setState({ user:{ ...this.state.user, name: event.target.value}})
     }
     // Function to set the User Email
-    setEmail(event) {
-        this.setState({ email: event.target.value });
+    setEmail = (event) => {
+        this.setState({ user:{ ...this.state.user, email: event.target.value}})
     }
     // Function to set the User Password
-    setPassword(event) {
-        this.setState({ password: event.target.value });
+    setPassword = (event) => {
+        this.setState({ user:{ ...this.state.user, password: event.target.value}})
     }
+    // Function to set the confirmation password
+    setConfirmPassword = (event) => {
+        this.setState({ confirmPassword: event.target.value })
+    }
+    /*
+        This function is responsible for updating the nameValidation state.
+    */
+    closeDialogN = () => {
+        this.setState({ nameValidation: false });
+    };
+    /*
+        This function is responsible for updating the emailValidation state.
+    */
+    closeDialogE = () => {
+        this.setState({ emailValidation: false });
+    };
+    /*
+        This function is responsible for updating the passwordValidation state.
+    */
+    closeDialogP = () => {
+        this.setState({ passwordValidation: false });
+    };
+    /*
+        This function is responsible for updating the passwordConfirmationValidation state.
+    */
+    closeDialogCP = () => {
+        this.setState({ passwordConfirmationValidation: false });
+    };
+    /* 
+        Function for chekcing to see if a email string is a valid email in the form of
+        <string>@<string>.<string>
+    */
+    emailValidationFunc(email)
+    {
+        let mailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        if (email.match(mailFormat))
+        {
+        return (true)
+        }
+        return (false)
+    }
+
     /* Function to send a POST request to the database (server), create a person object
         which contains the User's Name, User's Email and User's Password and POST to the route for
         handling user registration. Upon successful Registration, redirect to the Task's Page
         and send along the response.
     */
-    handleSubmit(event) {
-        event.preventDefault()
-        const person = {
-            name: this.state.name,
-            email: this.state.email,
-            password: this.state.password
-        };
+    handleSubmit = () => {
         const { history } = this.props;
-        Axios.post('http://localhost:5000/users', person)
-            .then(function(response) {
-                history.push({
-                    pathname:'/tasks',
-                    response
-                });
+        if(this.state.user.name.length < 2) {
+            this.setState({nameValidation: true});
+        }
+        else if(this.emailValidationFunc(this.state.user.email) === false) {
+            this.setState({emailValidation: true});
+        }
+        else if(this.state.user.password.length < 7) {
+            this.setState({passwordValidation: true});
+        }
+        else if(this.state.user.password !== this.state.confirmPassword) {
+            this.setState({passwordConfirmationValidation: true});
+        }
+        else {
+            Axios({
+                method: 'POST',
+                url: 'http://localhost:5000/users',
+                data: this.state.user
+            }).then(function(response) {
+                if(response.status === 200)
+                {
+                    history.push({pathname:'/tasks', response});
+                }
+            }).catch((error) =>
+            {
+                if(error.response.status === 400)
+                {
+                    this.setState({loginFailed: true});
+                }
             })
-            .catch(function(error) {});
-
-        this.setState({ name: "", email: "", password: "" })
+        }
     }
 
     render() {
         return ( 
         <div >
-            <form onSubmit = { this.handleSubmit} >
-                <label > 
-                    Name: <input type = "text" value = { this.state.name } onChange = { this.setName }/> 
-                </label > 
-                
-                <label >
-                    Email: <input type = "text" value = { this.state.email } onChange = { this.setEmail }/> 
-                </label> 
-                
-                <label >
-                    Password: <input type = "password" value = { this.state.password } onChange = { this.setPassword }/> 
-                </label>
-                
-                <label >
-                    Confirm Password: <input type = "password" />
-                </label> 
-                
-                <input type = "submit" value = "Submit" />
-            </form> 
+            <div>
+                <div>
+                    <TextField
+                        label="Name"
+                        style={{ margin: 8, width: '35ch' }}
+                        placeholder="Name"
+                        value={this.state.user.name}
+                        onChange={this.setName}
+                        margin="normal"
+                        type="text"/>
+                </div>
+                <div>
+                    <TextField
+                        label="Email"
+                        style={{ margin: 8, width: '35ch' }}
+                        placeholder="Email"
+                        value={this.state.user.email}
+                        onChange={this.setEmail}
+                        margin="normal"
+                        type="text"/>
+                </div>
+                <div>
+                    <TextField
+                        label="Password"
+                        style={{ margin: 8, width: '35ch' }}
+                        placeholder="Password"
+                        value={this.state.user.password}
+                        onChange={this.setPassword}
+                        margin="normal"
+                        type="password"/>
+                </div>
+                <div>
+                    <TextField
+                        label="Confirm Password"
+                        style={{ margin: 8, width: '35ch' }}
+                        placeholder="Confirm Password"
+                        value={this.state.confirmPassword}
+                        onChange={this.setConfirmPassword}
+                        margin="normal"
+                        type="password"/>
+                </div>
+                <Button onClick={() => this.handleSubmit()}>
+                    Register
+                </Button>
+            </div>
+
+            <Dialog open={this.state.nameValidation} onClose={this.closeDialogN} aria-labelledby="form-dialog-title">
+                <DialogTitle>Error</DialogTitle>
+                <DialogContent> 
+                    Name must be at least 2 characters long.
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={this.closeDialogN}>
+                        Ok.
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
+            <Dialog open={this.state.emailValidation} onClose={this.closeDialogE} aria-labelledby="form-dialog-title">
+                <DialogTitle>Error</DialogTitle>
+                <DialogContent> 
+                    Please enter a valid email address.
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={this.closeDialogE}>
+                        Ok.
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
+            <Dialog open={this.state.passwordValidation} onClose={this.closeDialogP} aria-labelledby="form-dialog-title">
+                <DialogTitle>Error</DialogTitle>
+                <DialogContent> 
+                    Password must be at least 7 characters long.
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={this.closeDialogP}>
+                        Ok.
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
+            <Dialog open={this.state.passwordConfirmationValidation} onClose={this.closeDialogCP} aria-labelledby="form-dialog-title">
+                <DialogTitle>Error</DialogTitle>
+                <DialogContent> 
+                    The passwords do not match, Please re-enter your passwords.
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={this.closeDialogCP}>
+                        Ok.
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div >
         )
     }
